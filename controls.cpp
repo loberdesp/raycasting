5f3820b891296abf527785d16529876d0218b194
loberdesp <siembab04@gmail.com>
2023-12-15 15:21:06 +0100

    pitch extended

--- controls.cpp
+++ controls.cpp
@@ -1,85 +1,108 @@
-controls::controls() {
-    std::cout << "Created Controls!" << std::endl;
+controls::controls()
+{
+	std::cout << "Created Controls!" << std::endl;
 }
 
-bool controls::eventSwitch() {
-    switch (e.type) {
-		case SDL_QUIT: {
+bool controls::eventSwitch()
+{
+	switch (e.type)
+	{
+	case SDL_QUIT:
+	{
+		return true;
+		break;
+	}
+	case SDL_KEYDOWN:
+	{
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_ESCAPE:
+		{
 			return true;
 			break;
 		}
-		case SDL_KEYDOWN: {
-			switch (e.key.keysym.sym) {
-				case SDLK_ESCAPE: {
-					return true;
-					break;
-				}
-				case SDLK_SPACE: {
-					std::cout << "jump" << std::endl;
-				}
-			}
+		case SDLK_SPACE:
+		{
+			std::cout << "jump" << std::endl;
 		}
-		case SDL_MOUSEBUTTONDOWN: {
-            if(e.button.button == SDL_BUTTON_LEFT) {
-                std::cout << "shoot" << std::endl;
-            }
-        }
-		default: {
-			break;
 		}
 	}
-    return false;
+	case SDL_MOUSEBUTTONDOWN:
+	{
+		if (e.button.button == SDL_BUTTON_LEFT)
+		{
+			std::cout << "shoot" << std::endl;
+		}
+	}
+	default:
+	{
+		break;
+	}
+	}
+	return false;
 }
 
-SDL_Event* controls::getEvent() {
-    return &e;
+SDL_Event *controls::getEvent()
+{
+	return &e;
 }
 
-void controls::kbHandle(player* P, map *M) {
+void controls::kbHandle(player *P, map *M)
+{
 	bool move = false;
-    if(keyboardState[SDL_SCANCODE_LEFT]) {
+	if (keyboardState[SDL_SCANCODE_LEFT])
+	{
 		P->addAngle(1);
 	}
-    if(keyboardState[SDL_SCANCODE_RIGHT]) {
+	if (keyboardState[SDL_SCANCODE_RIGHT])
+	{
 		P->addAngle(-1);
 	}
-	if(keyboardState[SDL_SCANCODE_UP]) {
-		if(P->getPitch() <= WINY) {
+	if (keyboardState[SDL_SCANCODE_UP])
+	{
+		if (P->getPitch() <= WINY)
+		{
 			P->addPitch(5);
 		}
 	}
-	if(keyboardState[SDL_SCANCODE_DOWN]) {
-		if(P->getPitch() > -WINY) {
+	if (keyboardState[SDL_SCANCODE_DOWN])
+	{
+		if (P->getPitch() > -WINY)
+		{
 			P->addPitch(-5);
 		}
 	}
 
-
-	if(keyboardState[SDL_SCANCODE_LSHIFT]) {
+	if (keyboardState[SDL_SCANCODE_LSHIFT])
+	{
 		std::cout << "sprint" << std::endl;
 	}
-	if(keyboardState[SDL_SCANCODE_LCTRL]) {
+	if (keyboardState[SDL_SCANCODE_LCTRL])
+	{
 		std::cout << "crouch" << std::endl;
 	}
 
-
-	if(keyboardState[SDL_SCANCODE_W]) {
+	if (keyboardState[SDL_SCANCODE_W])
+	{
 		bool X, Y;
 		M->checkCol(P->getX(), P->getY(), P->getVecX(), P->getVecY(), X, Y);
 		P->colMove(X, Y);
 	}
-	if(keyboardState[SDL_SCANCODE_S]) {
+	if (keyboardState[SDL_SCANCODE_S])
+	{
 		bool X, Y;
 		M->checkCol(P->getX(), P->getY(), -P->getVecX(), -P->getVecY(), X, Y);
 		P->colMoveBack(X, Y);
 	}
-	if(keyboardState[SDL_SCANCODE_A]) {
+	if (keyboardState[SDL_SCANCODE_A])
+	{
 		P->calcRot(90);
 		bool X, Y;
 		M->checkCol(P->getX(), P->getY(), P->getVecX(), P->getVecY(), X, Y);
 		P->colMove(X, Y);
 	}
-	if(keyboardState[SDL_SCANCODE_D]) {
+	if (keyboardState[SDL_SCANCODE_D])
+	{
 		P->calcRot(-90);
 		bool X, Y;
 		M->checkCol(P->getX(), P->getY(), P->getVecX(), P->getVecY(), X, Y);
@@ -87,28 +110,33 @@ void controls::kbHandle(player* P, map *M) {
 	}
 }
 
-void controls::mouseHandle(player* P, map *M) {
-	int k,u;
-	SDL_GetRelativeMouseState(&u,&k);
-	// if(P->getPitch() > -WINY && P->getPitch() <= WINY) {
-	// 	P->addPitch(float(-k)*SENSITIVITY);
-	// }
-	if(P->getPitch() > -WINY) {
-		P->addPitch(float(-k)*SENSITIVITY/2);
-	} else {
-		P->addPitch(-WINY-P->getPitch());
-		if(-k<0) {
-			P->addPitch(float(-k)*SENSITIVITY/2);
+void controls::mouseHandle(player *P, map *M)
+{
+	int k, u;
+	SDL_GetRelativeMouseState(&u, &k);
+	if (P->getPitch() > -2 * WINY)
+	{
+		P->addPitch(float(-k) * SENSITIVITY / 2);
+	}
+	else
+	{
+		P->addPitch(-2 * WINY - P->getPitch());
+		if (-k < 0)
+		{
+			P->addPitch(float(-k) * SENSITIVITY / 2);
 		}
 	}
-	if(P->getPitch() <= WINY) {
-		P->addPitch(float(-k)*SENSITIVITY/2);
-	} else {
-		P->addPitch(WINY-P->getPitch());
-		if(-k>0) {
-			P->addPitch(float(-k)*SENSITIVITY/2);
+	if (P->getPitch() <= 2 * WINY)
+	{
+		P->addPitch(float(-k) * SENSITIVITY / 2);
+	}
+	else
+	{
+		P->addPitch(2 * WINY - P->getPitch());
+		if (-k > 0)
+		{
+			P->addPitch(float(-k) * SENSITIVITY / 2);
 		}
 	}
-	P->addAngle(float(-u)*180/(3*WINY)*SENSITIVITY);
+	P->addAngle(float(-u) * 180 / (3 * WINY) * SENSITIVITY);
 }
-
